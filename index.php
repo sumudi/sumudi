93b58dcd2be139ce3b39f00f21b495c4836a9f3c
sumudi <sumudi433@gmail.com>
2024-10-13 12:17:58 +0530

    hhfdhfd

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
+<!DOCTYPE html>
 <html lang="en">
-    <head>
-        <title>
-</title>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
-    <body>
-        <div> test</div>
+<body>
+    <div>Test</div>
+    hdgsdggsddgsdgdgdghdsdhgd
 </body>
-
 </html>
